0e040718baf0277cd500bc3e778d0a3bfae49b8b
ah-nemati <nemati.amh@gmail.com>
2026-08-22 11:37:09 +0330

    feat: add @types/node to devDependencies and enhance Vite configuration for chunking

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,40 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    react({}),
+    tailwindcss(),
+  ],
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("leaflet") || id.includes("react-leaflet")) {
+              return "leaflet";
+            }
+
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router")
+            ) {
+              return "react";
+            }
+
+            if (id.includes("framer-motion")) {
+              return "animation";
+            }
+
+            if (id.includes("lucide-react")) {
+              return "icons";
+            }
+          }
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 1000,
+  },
 });
